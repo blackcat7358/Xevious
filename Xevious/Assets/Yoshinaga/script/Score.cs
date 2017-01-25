4218bcd6a898e9c6de997f0c0375661efdcf8dd0
unknown <nwuser@GM2150083.da.iwasaki.ac.jp>
2017-01-26 01:30:28 +0900

    タイトル画面完成

---
+++ Xevious/Assets/Yoshinaga/script/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public int score = 0;
+	public Text scoreText;
+
+	void Start(){
+		scoreText.text = "0";
+	}
+	void Update(){
+
+	}
+
+}
