2e8a7f25698887539028210f056dd777b489735d
unknown <nwuser@GM2150083.da.iwasaki.ac.jp>
2017-01-30 13:47:09 +0900

    タイトルシーンの修正

--- Xevious/Assets/Yoshinaga/script/Score.cs
+++ Xevious/Assets/Yoshinaga/script/Score.cs
@@ -8,11 +8,7 @@ public class Score : MonoBehaviour {
 	public Text scoreText;
 
 	void Start(){
-<<<<<<< HEAD
-		//scoreText.text = "0";
-=======
-		scoreText.text = "jbhfhcfcvh";
->>>>>>> 941fa967a14a89c797e7cc1cf17613cdb0612a18
+
 	}
 	void Update(){
 
